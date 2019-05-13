442c6c8a3d22ec32b59248fb9b4f91fe141c784b
therealplato <therealplato@gmail.com>
2019-05-14 00:55:02 +1200

    implement hash and clean up output

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"crypto/sha256"
 	"errors"
 	"fmt"
+	"hash"
+	"io"
 	"log"
 	"os"
 	"path/filepath"
@@ -11,15 +14,19 @@ import (
 var hashPath map[string]string
 var problemPaths []string
 var total int
+var hasher hash.Hash
 
 func main() {
 	hashPath = make(map[string]string)
 	problemPaths = make([]string, 0)
+	hasher = sha256.New() // Save some cpu cycles by not constructing this per file
+
 	err := filepath.Walk(".", perFileOrDir)
 	if err != nil {
 		// aborted early
 		log.Fatalf("stopping early due to issue: %q", err)
 	}
+
 	fmt.Printf("hashed %v files, finding %v unique hashes\n", total, len(hashPath))
 	if len(problemPaths) > 0 {
 		fmt.Printf("the following paths could not be accessed, maybe permission issue")
@@ -40,14 +47,19 @@ func perFileOrDir(path string, info os.FileInfo, err error) error {
 	}
 
 	total += 1
-	s := sum(path)
-	if s == "" {
+	bb := sum(path)
+	if len(bb) == 0 {
 		return errors.New("failed to hash " + path)
 	}
+
+	// make a hexadecimal string of the bytes:
+	s := fmt.Sprintf("%x", bb)
+
+	// check for collision:
 	existed, ok := hashPath[s]
 	if ok {
 		// dupe was already there
-		fmt.Printf("%q duplicates %q with hash %q\n", path, existed, sum)
+		fmt.Printf("%q duplicates %q with hash %q\n", path, existed, s)
 	} else {
 		// store key,value = sum,path
 		hashPath[s] = path
@@ -55,7 +67,18 @@ func perFileOrDir(path string, info os.FileInfo, err error) error {
 	return nil
 }
 
-// sum returns a string representation of the sha256 sum of the input path
-func sum(path string) string {
-	return ""
+// sum returns the sha256 sum of the input path
+func sum(path string) []byte {
+	f, err := os.Open(path)
+	if err != nil {
+		return nil
+	}
+	defer f.Close()
+
+	hasher.Reset()
+	if _, err := io.Copy(hasher, f); err != nil {
+		return nil
+	}
+
+	return hasher.Sum(nil) // the nil just means don't add any more data to the copied data
 }
